3bcfc0f932bdae33c029fa399e2099316e0f0a4f
tsuki-superior <tsukisuperior@tutanota.com>
2021-01-22 01:44:08 +0000

    changes and adding of arm

--- install-linux.sh
+++ install-linux.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+#TS/OS toolchain location
+TSOS_TOOLCHAIN=/usr/tsos-toolchain
+
 #Remove previous installation
-rm -rfv /usr/tsos-toolchain
+rm -rfv $TSOS_TOOLCHAIN
 
 #The version of gcc that will be used here
 GCC_VERSION=10.1.0
 
 #The version of binutils that will be used here
 BINUTILS_VERSION=2.35
-
-#The version of devkitarm that will be used here
-DEVKITARM_VERSION=r55
 
 #The version of gbdk to use
 GBDK_VERSION=4.0.2
@@ -23,9 +23,6 @@ GCC_DOWNLOAD_URL=https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.t
 
 #The url for binutils`s tarball
 BINUTILS_DOWNLOAD_URL=https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.xz
-
-#The url for devkitarms`s tarball
-DEVKITARM_DOWNLOAD_URL=https://github.com/devkitPro/buildscripts/archive/devkitARM_$DEVKITARM_VERSION.tar.gz
 
 #The sdcc download url
 SDCC_DOWNLOAD_URL=https://netactuate.dl.sourceforge.net/project/sdcc/sdcc/$SDCC_VERSION/sdcc-src-$SDCC_VERSION.tar.bz2
@@ -41,31 +38,33 @@ cd /tmp/tsos
 #Get the compilers from the web
 wget $GCC_DOWNLOAD_URL
 wget $BINUTILS_DOWNLOAD_URL
-#wget $DEVKITARM_DOWNLOAD_URL
 #wget $SDCC_DOWNLOAD_URL
 #wget $GBDK_DOWNLOAD_URL
 
 #Unarchive those tarballs
 tar -xf gcc-$GCC_VERSION.tar.xz
-mv -v gcc-$GCC_VERSION/ gcc/
+mv -v gcc-$GCC_VERSION/ gcc-x86/
+cp -Rv gcc-x86/ gcc-arm/
+
 tar -xf binutils-$BINUTILS_VERSION.tar.xz
-mv -v binutils-$BINUTILS_VERSION/ binutils/
-#tar -xf devkitARM_$DEVKITARM_VERSION.tar.gz
-#mv -v buildscripts-devkitARM_$DEVKITARM_VERSION/ devkitarm
+mv -v binutils-$BINUTILS_VERSION/ binutils-x86/
+cp -Rv binutils-x86/ binutils-arm/
+
 #tar -xf sdcc-src-$SDCC_VERSION.tar.bz2
 #mv -v sdcc-src-$SDCC_VERSION/ sdcc
+
 #tar -xf $GBDK_VERSION.tar.gz
 #mv -v gbdk-2020-$GBDK_VERSION/ gbdk
 
 #Lets update that path variable
-export PATH=$PATH:$INSTALL_DIR/bin
+export PATH=$PATH:$TSOS_TOOLCHAIN/bin
 echo "export PATH=$PATH" >>~/.bashrc
 
-#We will compile binutils first
-cd binutils
+#We will compile binutils first for x86
+cd binutils-x86
 mkdir -pv build
 cd build
-../configure --prefix=$INSTALL_DIR \
+../configure --prefix=$TSOS_TOOLCHAIN \
     --enable-gold \
     --enable-interwork \
     --target=i686-elf \
@@ -75,34 +74,63 @@ cd build
 
 make -j$(nproc)
 make install
-rm -rfv /tmp/tsos/binutils
+rm -rfv /tmp/tsos/binutils-x86
 
-#Now we will compile gcc
+#Now we will compile gcc for x86
 cd /tmp/tsos
-cd gcc
+cd gcc-x86
 mkdir -pv build
 cd build
-../configure --prefix=$INSTALL_DIR \
+../configure --prefix=$TSOS_TOOLCHAIN \
     --enable-languages=c,c++ \
     --disable-libssp \
     --disable-nls \
+    --target=i686-elf \
     --disable-libssp \
     --disable-werror \
     --without-headers
 
+make -j$(nproc)
+make install
+rm -rfv /tmp/tsos/gcc-x86
+
+#We will compile binutils for arm
+cd /tmp/tsos
+cd binutils-arm
+mkdir -pv build
+cd build
+../configure --prefix=$TSOS_TOOLCHAIN \
+    --enable-gold \
+    --enable-interwork \
+    --target=arm-none-eabi \
+    --enable-multilib \
+    --disable-nls \
+    --disable-werror
 
 make -j$(nproc)
 make install
-rm -rfv /tmp/tsos/gcc
+rm -rfv /tmp/tsos/binutils-arm
+
+#Now we will compile gcc for arm
+cd /tmp/tsos
+cd gcc-arm
+mkdir -pv build
+cd build
+../configure --prefix=$TSOS_TOOLCHAIN \
+    --enable-languages=c,c++ \
+    --disable-libssp \
+    --disable-nls \
+    --target=arm-none-eabi \
+    --disable-libssp \
+    --disable-werror \
+    --without-headers
+
+make -j$(nproc)
+make install
+rm -rfv /tmp/tsos/gcc-arm
 
 #Im sorry that stuff doesnt work rn
 exit
-
-#Install devkitarm
-cd /tmp/tsos
-cd devkitarm
-chmod +x ./*.sh
-./build-devkit.sh 1
 
 #Install sdcc
 cd /tmp/tsos
@@ -119,6 +147,6 @@ cd gbdk
 make 
 make install
 
-echo "export PATH=$PATH" >>~/.bashrc
+echo "export PATH=$PATH" >>~/.profile
 
 cd $CODE_DIR
